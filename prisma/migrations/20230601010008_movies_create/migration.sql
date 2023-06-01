b7a0c2873320a85fbc71c7d7c8d25bd550ae986e
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-05-31 22:10:19 -0300

    :construction: feat: tables created

---
+++ prisma/migrations/20230601010008_movies_create/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "movies" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "release_date" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "movies_title_key" ON "movies"("title");
---
+++ prisma/migrations/20230601010720_movie_rent_create/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "movie_rent" (
+    "userId" TEXT NOT NULL,
+    "movieId" TEXT NOT NULL,
+
+    PRIMARY KEY ("userId", "movieId"),
+    CONSTRAINT "movie_rent_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "movie_rent_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "movies" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
